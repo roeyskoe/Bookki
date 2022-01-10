44937512688c7fe6c21a0ed3bfe1578ee992cd48
Mikko <mikko.roysko@gmail.com>
2022-01-10 10:39:57 +0200

    Blazor & ASP.NET project

---
+++ Bookki/Server/BookCollection.cs
@@ -0,0 +1,29 @@
+﻿using Bookki.Shared;
+using System;
+using System.Collections.Generic;
+
+namespace Bookki.Server;
+
+public class BookCollection
+{
+    public List<Book> Books { get; private set; }
+
+    public BookCollection()
+    {
+        Books = new List<Book>();
+        FillWithDummyData(5);
+    }
+
+    private void FillWithDummyData(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            var book = new Book();
+            book.Name = Random.Shared.NextInt64().ToString();
+            book.Description = Random.Shared.NextInt64().ToString();
+            book.Author = Random.Shared.NextInt64().ToString();
+            Books.Add(book);
+        }
+    }
+}
+
---
+++ Bookki/Server/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bookki.Shared;
+using System.Collections.Generic;
+
+namespace Bookki.Server.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class BookController : Controller
+    {
+        private BookCollection books;
+
+        public BookController(BookCollection _books)
+        {
+            books = _books;
+        }
+
+        [HttpGet("all")]
+        public IEnumerable<Book> GetAll()
+        {
+            return books.Books;
+        }
+    }
+}
